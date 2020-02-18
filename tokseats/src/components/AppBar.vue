cab4717344827598b61f707dacc8a337c7003ba0
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-02-18 13:56:48 +0100

    Created AppBar.vue, Footer.vue, and Menu.vue

---
+++ tokseats/src/components/AppBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-app-bar color="white">
+      <v-btn icon text to="/" color="primary">
+        <v-img
+          :src="require('../assets/tokseatslogo.svg')"
+          max-height="40"
+          max-width="40"
+          contain
+        ></v-img>
+      </v-btn>
+
+      <v-toolbar-title>Toks Eats</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+};
+</script>
---
+++ tokseats/src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer color="teal">
+    <v-row justify="center">
+      <v-card flat tile class="white--text text-center" color="teal">
+        <v-card-text class="white--text pt-2=4">
+          This is applicaiton is built for demostration purposes only. Designed
+          with Adobe XD. Developed with Vue.Js, Vuetify and Firebase.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} —
+          <strong>Kehinde Akeredolu</strong>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
