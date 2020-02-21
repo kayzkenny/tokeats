af7385b2be52bb6060cfb43f00afb33dba101054
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-02-21 09:57:52 +0100

    Removed Skeleton Loader

--- tokseats/src/components/Menu.vue
+++ tokseats/src/components/Menu.vue
@@ -4,40 +4,22 @@
       <v-row justify="center">
         <p class="my-2 mx-auto display-1">Meals</p>
       </v-row>
-      <v-sheet
-        v-if="!menuLoaded"
-        :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
-        class="px-3 py-3"
-      >
-        <v-row justify="center">
-          <v-col cols="12" lg="3" md="4" sm="6">
-            <v-skeleton-loader class="mx-2" max-width="320" type="card"></v-skeleton-loader>
-          </v-col>
-          <v-col cols="12" lg="3" md="4" sm="6">
-            <v-skeleton-loader class="mx-2" max-width="320" type="card"></v-skeleton-loader>
-          </v-col>
-          <v-col cols="12" lg="3" md="4" sm="6">
-            <v-skeleton-loader class="mx-2" max-width="320" type="card"></v-skeleton-loader>
-          </v-col>
-          <v-col cols="12" lg="3" md="4" sm="6">
-            <v-skeleton-loader class="mx-2" max-width="320" type="card"></v-skeleton-loader>
-          </v-col>
+      <transition name="slide-fade">
+        <v-row class="my-4" justify="center">
+          <v-card max-width="250" v-for="meal in meals" :key="meal.name" class="ma-4">
+            <v-img height="220" :src="meal.imageUrl"></v-img>
+            <v-card-title>&#8358;{{ meal.price }}</v-card-title>
+            <v-card-subtitle>{{ meal.name }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn text color="primary">Add to Cart</v-btn>
+            </v-card-actions>
+          </v-card>
         </v-row>
-      </v-sheet>
-      <v-row v-if="menuLoaded" class="my-4" justify="center">
-        <v-card max-width="250" v-for="meal in meals" :key="meal.name" class="ma-4">
-          <v-img height="220" :src="meal.imageUrl"></v-img>
-          <v-card-title>&#8358;{{ meal.price }}</v-card-title>
-          <v-card-subtitle>{{ meal.name }}</v-card-subtitle>
-          <v-card-actions>
-            <v-btn text color="primary">Add to Cart</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-row>
+      </transition>
       <v-row justify="center">
         <p class="my-2 mx-2 display-1">Singles</p>
       </v-row>
-      <v-row v-if="menuLoaded" class="my-4" justify="center">
+      <v-row class="my-4" justify="center">
         <v-card max-width="250" v-for="single in singles" :key="single.name" class="ma-4">
           <v-img height="220px" :src="single.imageUrl"></v-img>
           <v-card-title>&#8358;{{ single.price }}</v-card-title>
@@ -50,7 +32,7 @@
       <v-row justify="center">
         <p class="my-2 mx-2 display-1">Sides</p>
       </v-row>
-      <v-row v-if="menuLoaded" class="my-4" justify="center">
+      <v-row class="my-4" justify="center">
         <v-card max-width="250" v-for="side in sides" :key="side.info" class="ma-4">
           <v-img height="220px" :src="side.imageUrl"></v-img>
           <v-card-title>&#8358;{{ side.price }}</v-card-title>
@@ -63,7 +45,7 @@
       <v-row justify="center">
         <p class="my-2 mx-2 display-1">Drinks</p>
       </v-row>
-      <v-row v-if="menuLoaded" class="my-4" justify="center">
+      <v-row class="my-4" justify="center">
         <v-card max-width="250" v-for="drink in drinks" :key="drink.name" class="ma-4">
           <v-img height="220px" :src="drink.imageUrl"></v-img>
           <v-card-title>&#8358;{{ drink.price }}</v-card-title>
@@ -81,20 +63,28 @@
 import { mapGetters } from "vuex";
 export default {
   name: "Menu",
-  data: () => ({
-    menuLoaded: false
-  }),
   computed: {
     ...mapGetters("menu", ["meals", "singles", "sides", "drinks"])
   },
   created() {
-    this.$store
-      .dispatch("menu/loadMenuAction")
-      .catch(error => console.log(error)); // get menu state
-  },
-  updated() {
-    this.menuLoaded = true;
+    this.$store.dispatch("menu/loadMenuAction").catch(error => alert(error)); // get menu state
   },
   inject: ["theme"]
 };
 </script>
+
+<style>
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
