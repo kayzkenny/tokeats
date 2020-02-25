c2b97f2c41a847637008b6350a40e3b7c3b96913
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-02-25 07:42:39 +0100

    Created Dashboard.vue and AppBar.vue

---
+++ admin/src/components/AppBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar color="white">
+      <v-btn icon text to="/">
+        <v-img :src="require('../assets/tokseatslogo.svg')" max-height="40" max-width="40"></v-img>
+      </v-btn>
+      <v-toolbar-title>Toks Eats Admin</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppBar"
+};
+</script>
